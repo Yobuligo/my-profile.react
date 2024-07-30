aa470ddd6de3f06047c200507c6823d10ba5565d
Yobuligo <hoffmannpeter82@gmail.com>
2024-07-30 19:49:07 +0200

    Introduce environment variables

--- src/api/ProjectApi.ts
+++ src/api/ProjectApi.ts
@@ -1,3 +1,4 @@
+import { AppConfig } from "../AppConfig";
 import { IProject, ProjectMeta } from "../model/IProject";
 import { DateTime } from "../services/date/DateTime";
 import { Repository } from "./core/Repository";
@@ -8,7 +9,9 @@ export class ProjectApi extends Repository<IProject> {
   }
 
   async findAll(): Promise<IProject[]> {
-    const projects = await this.get<IProject[]>("/data/projects.json");
+    const projects = await this.get<IProject[]>(
+      `${AppConfig.HOST}/data/projects.json`
+    );
     return projects.sort((left, right) =>
       DateTime.compare(right.timeInterval.to, left.timeInterval.to)
     );
