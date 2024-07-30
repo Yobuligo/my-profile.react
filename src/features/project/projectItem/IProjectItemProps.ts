7392d36d20e7254ae3ddf70470a154b0b83bb8f1
Yobuligo <hoffmannpeter82@gmail.com>
2024-07-30 15:31:39 +0200

    Add styling to TabStrips

--- src/features/project/projectItem/IProjectItemProps.ts
+++ src/features/project/projectItem/IProjectItemProps.ts
@@ -1,6 +1,7 @@
 import { IProject } from "../../../model/IProject";
 
 export interface IProjectItemProps {
+  className?: string;
   onSelect?: () => void;
   project: IProject;
 }
--- src/features/project/projectItem/ProjectItem.tsx
+++ src/features/project/projectItem/ProjectItem.tsx
@@ -1,4 +1,5 @@
 import { Card } from "../../../components/card/Card";
+import { style } from "../../../core/utils/style";
 import { CalendarIcon } from "../../../icons/CalendarIcon";
 import { DateTime } from "../../../services/date/DateTime";
 import { RoleList } from "../../role/roleList/RoleList";
@@ -7,7 +8,10 @@ import styles from "./ProjectItem.module.scss";
 
 export const ProjectItem: React.FC<IProjectItemProps> = (props) => {
   return (
-    <Card className={styles.projectItem} onClick={props.onSelect}>
+    <Card
+      className={style(styles.projectItem, props.className)}
+      onClick={props.onSelect}
+    >
       <RoleList roles={props.project.roles} />
       <div className={styles.header}>
         <h3>{props.project.title}</h3>
