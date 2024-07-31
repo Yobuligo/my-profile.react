6348fce716eb392f1663a4ab91e5f5f8eab2f9fc
Yobuligo <hoffmannpeter82@gmail.com>
2024-07-31 09:05:32 +0200

    Extract LanguageSelect to separate component

--- src/components/pages/pageHeader/PageHeader.tsx
+++ src/components/pages/pageHeader/PageHeader.tsx
@@ -1,38 +1,12 @@
-import { useMemo } from "react";
-import { useLanguage } from "../../../hooks/useLanguage";
-import { Language } from "../../../types/Language";
-import { ISelectOption } from "../../select/ISelectOption";
-import { Select } from "../../select/Select";
+import { LanguageSelect } from "../../languageSelect/LanguageSelect";
 import { IPageHeaderProps } from "./IPageHeaderProps";
 import styles from "./PageHeader.module.scss";
 
 export const PageHeader: React.FC<IPageHeaderProps> = (props) => {
-  const [language, setLanguage] = useLanguage();
-
-  const languageOptions = useMemo<ISelectOption[]>(
-    () => [
-      { key: "de", text: "Deutsch" },
-      { key: "en", text: "English" },
-    ],
-    []
-  );
-
-  const selectedLanguageOption = languageOptions.find(
-    (languageOption) => languageOption.key === language
-  );
-
-  const onSelect = (option: ISelectOption) => {
-    setLanguage(option.key as Language);
-  };
-
   return (
     <header className={styles.pageHeader}>
       <div className={styles.select}>
-        <Select
-          onSelect={onSelect}
-          options={languageOptions}
-          selected={selectedLanguageOption}
-        />
+        <LanguageSelect />
       </div>
     </header>
   );
--- src/components/select/ISelectProps.ts
+++ src/components/select/ISelectProps.ts
@@ -1,7 +1,8 @@
 import { IHaveClassName } from "../../core/types/IHaveClassName";
 import { ISelectOption } from "./ISelectOption";
 
-export interface ISelectProps<T extends ISelectOption> extends IHaveClassName {
+export interface ISelectProps<T extends ISelectOption<any>>
+  extends IHaveClassName {
   onSelect?: (option: T) => void;
   options: T[];
   selected?: T;
