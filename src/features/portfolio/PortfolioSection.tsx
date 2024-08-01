97b9d01f005ec143b0a8348f7a022b6e9b35daeb
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-01 11:02:35 +0200

    Extend Portfolio

--- src/features/portfolio/PortfolioSection.tsx
+++ src/features/portfolio/PortfolioSection.tsx
@@ -1,10 +1,11 @@
 export const PortfolioSection: React.FC = () => {
   return (
     <>
-      <div>Business Software Development</div>
+      <div>Web based Business Software Development</div>
       <div>Development Coaching</div>
       <div>Scrum Coaching</div>
       <div>Taekwon-Do</div>
+      <div>Problemlöser</div>
     </>
   );
 };
